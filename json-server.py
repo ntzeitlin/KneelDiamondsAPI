ad204a3368f286666e5e3c2d0e53ac3edccfc91d
Nate Z <ntzeitlin@gmail.com>
2025-02-24 11:37:08 -0600

    started get_all_orders functionality

---
+++ json-server.py
@@ -0,0 +1,19 @@
+import json
+from http.server import HTTPServer
+from nss_handler import HandleRequests, status
+
+from views import get_all_orders
+
+
+class JSONServer(HandleRequests):
+    """Server class to handle incoming HTTP requests for kneel diamonds"""
+
+    def do_GET(self):
+        """Handle GET requests from a client"""
+
+        response_body = ""
+        url = self.parse_url(self.path)
+
+        if url["requested_resource"] == "orders":
+            response_body = get_all_orders()
+            return self.response(response_body, status.HTTP_200_SUCCESS.value)
---
+++ views/orders_view.py
@@ -0,0 +1,6 @@
+import sqlite3
+import json
+
+
+def get_all_orders():
+    pass

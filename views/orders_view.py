062d70bcfeed4933db72cf21ee4c7e586b96bed5
Nate Z <ntzeitlin@gmail.com>
2025-02-24 14:16:23 -0600

    users can now GET info on a single order

--- views/orders_view.py
+++ views/orders_view.py
@@ -34,3 +34,38 @@ def get_all_orders():
 
         serialized_orders = json.dumps(orders)
     return serialized_orders
+
+
+def get_single_ship(pk):
+    with sqlite3.connect("./kneeldiamonds.sqlite3") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute(
+            """
+            SELECT
+                orders.id,
+                metals.id,
+                metals.metal,
+                sizes.id,
+                sizes.size,
+                styles.id,
+                styles.style
+
+                FROM Orders orders
+                JOIN Metals metals
+                    ON metals.id = orders.metal_id
+                JOIN Sizes sizes
+                    ON sizes.id = orders.size_id
+                JOIN Styles styles
+                    ON styles.id = orders.style_id
+                WHERE orders.id = ?
+                    """,
+            (pk,),
+        )
+        query_results = db_cursor.fetchone()
+        dictionary_version_of_object = dict(query_results) if query_results else {}
+
+        # Serialize Python list to JSON encoded string
+        serialized_order = json.dumps(dictionary_version_of_object)
+    return serialized_order
